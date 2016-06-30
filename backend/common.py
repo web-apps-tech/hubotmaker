edd0d06d9ff627f3b7d1e227def7c067c1c119ad
nasa9084 <nasa9084@live.jp>
2016-07-01 00:46:08 +0900

    add: Hubot class and some methods
    create, __init__, start, stop, remove

--- backend/common.py
+++ backend/common.py
@@ -1,2 +1,77 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
+
+import json
+import requests
+from uuid import uuid4
+
+import config
+
+
+class Hubot(object):
+    def __init__(self, name, db):
+        self.name = name
+        self.db = db
+
+    @classmethod
+    def create(cls, slack_token):
+        endpoint = '/containers/create'
+        name = str(uuid4())
+        db = str(uuid4())
+        redis_payload = {
+            'Image': 'redis'
+        }
+        hubot_payload = {
+            'Image': 'hubot',
+            'Env': [
+                'HUBOT_SLACK_TOKEN={0}'.format(slack_token)
+            ],
+            'HostConfig': {
+                'Links': [db + ":db"]
+            }
+        }
+        requests.post(
+            config.DOCKER_BASEURI + endpoint + '?name={}'.format(db),
+            headers={
+                'Content-type': 'application/json'
+            },
+            data=json.dumps(redis_payload)
+        )
+        requests.post(
+            config.DOCKER_BASEURI + endpoint + '?name={}'.format(name),
+            headers={
+                'Content-type': 'application/json'
+            },
+            data=json.dumps(hubot_payload)
+        )
+        return cls(name, db)
+
+    def remove(self):
+        endpoint = '/containers/{0}'
+        self.last_response = requests.delete(
+            config.DOCKER_BASEURI + endpoint.format(self.db) + '?force=True'
+        )
+        if self.last_response.status_code == 204:
+            self.last_response = requests.delete(
+                config.DOCKER_BASEURI + endpoint.format(self.name) + '?force=True'
+            )
+
+    def start(self):
+        endpoint = '/containers/{0}/start'
+        self.last_response = requests.post(
+            config.DOCKER_BASEURI + endpoint.format(self.db)
+        )
+        if self.last_response.status_code == 204:
+            self.last_response = requests.post(
+                config.DOCKER_BASEURI + endpoint.format(self.name)
+            )
+
+    def stop(self):
+        endpoint = '/containers/{0}/stop'
+        self.last_response = requests.post(
+            config.DOCKER_BASEURI + endpoint.format(self.db)
+        )
+        if self.last_response.status_code == 204:
+            self.last_response = requests.post(
+                config.DOCKER_BASEURI + endpoint.format(self.name)
+            )
