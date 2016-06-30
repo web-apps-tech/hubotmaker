abb1440b51086b884fb1ad88a6059270c1bb1796
nasa9084 <nasa9084@live.jp>
2016-07-01 01:07:18 +0900

    add: some config

--- backend/config.py
+++ backend/config.py
@@ -1,4 +1,11 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 
+DBINFO = {
+    'host': 'localhost',
+    'db': '',
+    'user': '',
+    'passwd': ''
+}
+APIKEY = "apikey"
 DOCKER_BASEURI = 'http://localhost:2375'
