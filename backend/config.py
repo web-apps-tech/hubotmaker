171e7d0ebd69641631506d6a0681f14d952052a6
nasa9084 <nasa9084@live.jp>
2016-07-01 14:29:55 +0900

    update: remove DBINFO config
    now not used

--- backend/config.py
+++ backend/config.py
@@ -1,12 +1,6 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 
-DBINFO = {
-    'host': 'localhost',
-    'db': '',
-    'user': '',
-    'passwd': ''
-}
 APIKEY = "apikey"
 DOCKER_BASEURI = 'http://localhost:2375'
 DOCKER_HUBOT_IMAGE = 'hubot:latest'
