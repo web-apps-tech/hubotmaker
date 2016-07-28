35c4fc83dcce85638a70d8f06a105c93697768e6
chamaharun <chamaharun@outlook.com>
2016-07-28 22:05:57 +0900

    login

--- frontend/login.js
+++ frontend/login.js
@@ -1,6 +1,32 @@
 $(document).ready(function() {
     var SESSID = $.cookie("SESSID");
-    if(SESSID !== undefined){
+    if(SESSID !== undefined){    
 	document.href = "../list/";
     }
 });
+$(".LoginButton").on("click",function(){
+    $.ajax({
+        type: "POST",
+	url: ApiEndPoint + "/user/apikey",
+	DataType: "json",
+        data: {
+	    username: $("#Username").val(),
+	    password: $("#Password").val()
+	
+	},
+	success: function(data){
+	    if(data.status){
+	        $.cookie("SESSID",data.apikey);
+		document.href = "../list/";
+	    }else{
+	        LoginError();
+	    }
+	}
+    });
+
+});
+
+function LoginError(){
+    console.log("error");
+    //ろぐいんえら
+}
