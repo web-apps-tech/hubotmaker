906d0cc375444490d8dc5b260b6cb7b8fa74154d
chamaharun <chamaharun@outlook.com>
2016-07-29 00:15:29 +0900

    tbody 🆎

--- frontend/list.js
+++ frontend/list.js
@@ -1,4 +1,21 @@
 var ApiEndPoint = "http://133.242.53.17/";
+
+function generateTbody(hubotId){
+var listHTML = "\n\
+                        <tr>\n\ 
+                            <td>" + hubotId + "</td>\n\
+                            <td>OFF</td>\n\
+                            <td>\n\
+                                <button class=\"btn btn-default edit\">Edit</button>\n\
+                                <button class=\"btn btn-info\">Start</button>\n\
+                                <button class=\"btn btn-warning hidden\">Stop</button>\n\
+                            </td>\n\
+                        </tr>\n\
+			"
+    return(listHTML);
+}
+
+
 $(document).ready(function(){
     var SESSID = $.cookie("SESSID");
     if(SESSID !== undefined){
@@ -13,7 +30,8 @@ $(document).ready(function(){
 	            console.log(data.status);
 		    var hubotIds = data.message;
 		    for (var i=0; i < hubotIds.length; i++){
-		        console.log(hubotIds[i]);
+                        $(".hubot-list-tbody").append(generateTbody(hubotIds[i]);
+   			console.log(hubotIds[i]);
 		    } 
 	        }
         });
