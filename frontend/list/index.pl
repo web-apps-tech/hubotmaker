9a0fb8828b663c6121deb9385f7dac65563d221b
chamaharun <chamaharun@outlook.com>
2016-07-28 20:27:37 +0900

    created index.pl

--- frontend/list/index.pl
+++ frontend/list/index.pl
@@ -1,1 +1,15 @@
-../index.pl
+#!/usr/bin/perl
+
+use HTML::Template;
+use CGI qw/:standard/;
+
+
+
+my $template = HTML::Template->new(filename => 'index.tmpl',
+                                'die_on_bad_params' => 0
+                                );
+
+$template->param(APIKEY => param("api-key") );
+
+print "Content-Type: text/html\nAccess-Control-Allow-Origin: *\n\n", $template->output;
+
