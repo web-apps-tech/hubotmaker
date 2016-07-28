22a5cc91b80510fe4b7e000041f63c6b06120364
chamaharun <chamaharun@outlook.com>
2016-07-28 16:52:54 +0900

    first commit

---
+++ frontend/script.js
@@ -0,0 +1,60 @@
+var ApiEndPoint = "http://133.242.53.17/";
+
+$(".create-new").on("click", function() {
+    $('#CreateModal').modal("show")
+});
+$(".edit").on("click", function() {
+    $('#EditModal').modal("show")
+});
+$(".delete").on("click", function() {
+    $('#DeleteConfirm').modal("show")
+});
+$(".UserRegistSubmit").on("click", UserRegistSubmit);
+
+
+
+function UserRegistSubmit() {
+
+    $.ajax({
+        type: "POST",
+        url: ApiEndPoint + "user",
+        cache: false,
+        data: {
+            username: $("#Username").val(),
+            password: $("#Password").val()
+        },
+        processData: true,
+    }).done(function(data) {
+        if (data.status == "ture") {
+            alert("submitted")
+            UserActivate();
+        } else {
+            $("#Username").addClass("has-error");
+            $("#Password").addClass("has-error");
+        }
+    });
+}
+
+function UserActivate() {
+
+    $.ajax({
+        type: "PUT",
+        url: ApiEndPoint + "user/activate",
+        cache: false,
+        data: {
+            username: $("#Username").val(),
+            password: $("#Password").val()
+        },
+        processData: true,
+    }).done(function(data) {
+        if (data.status == "ture") {
+            RegisterCompleted();
+        }
+    });
+}
+
+function RegisterCompleted() {
+    $("RegisterCompleted").removeClass("hidden");
+}
+
+//$(".UserRegistSubmit").on("click",UserRegistSubmit());
