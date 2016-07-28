f73878444cbb0e6dd65578a8e4ec4c45e8a4b546
chamaharun <chamaharun@outlook.com>
2016-07-28 21:52:10 +0900

    delete forceful redirect when dont have param(api-key)

--- frontend/list/index.pl
+++ frontend/list/index.pl
@@ -5,20 +5,15 @@ use CGI qw/:standard/;
 
 my $api_key = param("api-key");
 
-if ($api_key ne ""){
-    my $template = HTML::Template->new(filename => 'index.tmpl',
-                                    'die_on_bad_params' => 0
-                                    );
+my $template = HTML::Template->new(filename => 'index.tmpl',
+                                'die_on_bad_params' => 0
+                                );
 
-    $template->param(APIKEY => param("api-key") );
-    print header(-type => "text/html",
-	         -status => 200,
-		 -charset=>'utf-8',
-		 -Access_Control_Allow_Origin=>'*'),
-	  $template->output;
-
-  }else{
-    print header(-Location => "../login" , -status => 302);
-}
+$template->param(APIKEY => param("api-key") );
+print header(-type => "text/html",
+             -status => 200,
+    	 -charset=>'utf-8',
+    	 -Access_Control_Allow_Origin=>'*'),
+      $template->output;
 
 
