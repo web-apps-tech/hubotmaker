437828c683a81a25c2f4b25fcbe4251f4b90826b
chamaharun <chamaharun@outlook.com>
2016-07-28 21:10:06 +0900

    Login check

--- frontend/list/index.pl
+++ frontend/list/index.pl
@@ -3,13 +3,22 @@
 use HTML::Template;
 use CGI qw/:standard/;
 
+my $api_key = param("api-key");
 
+if ($api_key ne ""){
+    my $template = HTML::Template->new(filename => 'index.tmpl',
+                                    'die_on_bad_params' => 0
+                                    );
 
-my $template = HTML::Template->new(filename => 'index.tmpl',
-                                'die_on_bad_params' => 0
-                                );
+    $template->param(APIKEY => param("api-key") );
+    print header(-type => "text/html",
+	         -status => 200,
+		 -charset=>'utf-8',
+		 -Access_Control_Allow_Origin=>'*'),
+	  $template->output;
 
-$template->param(APIKEY => param("api-key") );
+  }else{
+    print header(-Location => "../login" , -status => 302);
+}
 
-print "Content-Type: text/html\nAccess-Control-Allow-Origin: *\n\n", $template->output;
 
