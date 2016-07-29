fe6b52f2b43872a6d1e9babc8d9acb928c21a533
chamaharun <chamaharun@outlook.com>
2016-07-29 18:24:47 +0900

    input

--- frontend/list.js
+++ frontend/list.js
@@ -6,7 +6,7 @@ $(".delete").on("click", function() {
 });
 $("#create-submit").on("click",function(){
   console.log($("#SlackToken").val());
-  console.log($("#create-functions").children());
+  console.log($("#create-functions .checkbox .checkbox").prop());
 });
 
 var ApiEndPoint = "http://133.242.53.17";
