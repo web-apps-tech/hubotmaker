bb63bf53be3b3c1d988618d2c3969de24ddec1be
chamaharun <chamaharun@outlook.com>
2016-07-29 18:42:47 +0900

    👀

--- frontend/list.js
+++ frontend/list.js
@@ -8,7 +8,8 @@ $("#create-submit").on("click",function(){
   console.log($("#SlackToken").val());
   var tags = $("#create-functions .checkbox label");
   for(var i=0; i < tags.length; i++){
-      console.log(tags[i].children[0].val());
+    var checkbox = tags[i].children[0];
+      console.log(checkbox.val());
   }
 
 });
