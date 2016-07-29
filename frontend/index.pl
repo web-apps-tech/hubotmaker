d6d12e491ebbec2e15a4271e0757c1c1bd6aa9f5
chamaharun <chamaharun@outlook.com>
2016-07-29 22:42:03 +0900

    CORS del

--- frontend/index.pl
+++ frontend/index.pl
@@ -6,5 +6,4 @@ use HTML::Template;
 my $template = HTML::Template->new(filename => 'index.tmpl',
                                 'die_on_bad_params' => 0
                                 );
-print "Content-Type: text/html\nAccess-Control-Allow-Origin: *\n\n", $template->output;
-
+print "Content-Type: text/html\n\n", $template->output;
