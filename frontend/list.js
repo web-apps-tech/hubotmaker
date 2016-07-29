d61569a0fa4a929d7766d0debf5d932cc3bc3316
chamaharun <chamaharun@outlook.com>
2016-07-29 14:41:11 +0900

    children test　🐻

--- frontend/list.js
+++ frontend/list.js
@@ -103,7 +103,7 @@ $(document).ready(function() {
                 }
                 $(".edit").on("click", function(e) {
                     console.log(e);
-                    console.log(e.target.parentNode.parentNode.children);
+                    console.log(e.target.parentNode.parentNode.children[0]);
                     $('#EditModal').modal("show");
                 });
                 setAvailableScripts();
