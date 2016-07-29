9dd6fbe3fe239138e93b5e32adb65646801944e5
chamaharun <chamaharun@outlook.com>
2016-07-29 17:39:05 +0900

    add ;

--- frontend/operations.js
+++ frontend/operations.js
@@ -1,21 +1,21 @@
 var ApiEndPoint = "http://133.242.53.17";
 
-function strtHubot(APIKey,hubotId){
-  $.ajax({
-  type: "POST",
-  url: ApiEndPoint "/hubot/"+ hubotId + "/start",
-  data: {
-    apikey: APIKey
-  },
-  dataType: "json",
-  success: function(data){
-    if(data.status){
-      alert("started");
-    }
-  }
-  });
+function strtHubot(APIKey, hubotId) {
+    $.ajax({
+        type: "POST",
+        url: ApiEndPoint "/hubot/" + hubotId + "/start",
+        data: {
+            apikey: APIKey
+        },
+        dataType: "json",
+        success: function(data) {
+            if (data.status) {
+                alert("started");
+            }
+        }
+    });
 }
 
-$(".start").on("click",function(e){
-  console.log(e.target.id);
-})
+$(".start").on("click", function(e) {
+    console.log(e.target.id);
+});
