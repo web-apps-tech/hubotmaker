0903c93d17404c571824f9be5d1fdf3d31e6b6c0
chamaharun <chamaharun@outlook.com>
2016-07-29 18:05:53 +0900

    add stop

--- frontend/list.js
+++ frontend/list.js
@@ -10,23 +10,23 @@ var ApiEndPoint = "http://133.242.53.17";
 
 
 function getHubotEnvs(APIKey, hubotId) {
-  var envs;
-  $.ajax({
-    type: "GET",
-    url: ApiEndPoint + "/hubot/" + hubotId + "/env",
-    data: {
-      apikey: APIKey,
-    },
-    dataType: "json",
-    async: false,
-    success: function(data){
-      if (data.status){
-        envs = data.message;
-      }
-    }
+    var envs;
+    $.ajax({
+        type: "GET",
+        url: ApiEndPoint + "/hubot/" + hubotId + "/env",
+        data: {
+            apikey: APIKey,
+        },
+        dataType: "json",
+        async: false,
+        success: function(data) {
+            if (data.status) {
+                envs = data.message;
+            }
+        }
 
-  });
-  return envs;
+    });
+    return envs;
 }
 
 
@@ -44,7 +44,7 @@ function generateTbody(hubotId) {
     return (listHTML);
 }
 
-function generateCheckboxes(prefix,scriptName) {
+function generateCheckboxes(prefix, scriptName) {
     var divHTML = "\n"
 
     divHTML += "<div class=\"checkbox\">\n";
@@ -93,7 +93,7 @@ function setAvailableScripts() {
         success: function(data) {
             if (data.status) {
                 for (var i = 0; i < data.message.length; i++) {
-                    $("#create-functions").append(generateCheckboxes("create",data.message[i]));
+                    $("#create-functions").append(generateCheckboxes("create", data.message[i]));
                     $("#edit-functions").append(generateCheckboxes("edit", data.message[i]));
                 }
             }
@@ -118,25 +118,28 @@ $(document).ready(function() {
                     $(".hubot-list-tbody").append(generateTbody(hubotIds[i]));
                     getStatus(SESSID, hubotIds[i]);
                     $(".start").on("click", function(e) {
-                        startHubot(SESSID,e.target.id.split("_")[1]);
+                        startHubot(SESSID, e.target.id.split("_")[1]);
+                    });
+                    $(".stop").on("click", function(e) {
+                        startHubot(SESSID, e.target.id.split("_")[1]);
                     });
                 }
                 $(".edit").on("click", function(e) {
                     var hubotId = e.target.parentNode.parentNode.children[0].textContent;
-                    var hubotEnvs = getHubotEnvs(SESSID,hubotId);
+                    var hubotEnvs = getHubotEnvs(SESSID, hubotId);
                     var slackToken = hubotEnvs["HUBOT_SLACK_TOKEN"];
                     $('#EditModal').modal("show");
                     $("#EditSlackToken").val(slackToken);
                     var checkboxes = $("#edit-functions .checkbox label");
-                    for (var i=0; i < checkboxes.length; i++){
-                      var scriptName = checkboxes[i].id.split('_')[1];
-                      console.log("scriptName:" + scriptName);
-                      var checkboxbId = "#" + "edit_" + scriptName + "_cb";
-                      if(hubotEnvs[scriptName]){
-                        $(checkboxbId).prop("checked",true);
-                      }else{
-                        $(checkboxbId).prop("checked",false);
-                      }
+                    for (var i = 0; i < checkboxes.length; i++) {
+                        var scriptName = checkboxes[i].id.split('_')[1];
+                        console.log("scriptName:" + scriptName);
+                        var checkboxbId = "#" + "edit_" + scriptName + "_cb";
+                        if (hubotEnvs[scriptName]) {
+                            $(checkboxbId).prop("checked", true);
+                        } else {
+                            $(checkboxbId).prop("checked", false);
+                        }
 
                     }
 
--- frontend/operations.js
+++ frontend/operations.js
@@ -15,3 +15,19 @@ function startHubot(APIKey, hubotId) {
         }
     });
 }
+
+function stopHubot(APIKey, hubotId) {
+    $.ajax({
+        type: "POST",
+        url: ApiEndPoint + "/hubot/" + hubotId + "/stop",
+        data: {
+            apikey: APIKey
+        },
+        dataType: "json",
+        success: function(data) {
+            if (data.status) {
+                getStatus(APIKey, hubotId);
+            }
+        }
+    });
+}
