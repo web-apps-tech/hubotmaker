6fc4081708595bb7b9ccb12a2b47148a43079857
chamaharun <chamaharun@outlook.com>
2016-07-29 22:21:05 +0900

    user-delete

--- frontend/script.js
+++ frontend/script.js
@@ -3,8 +3,10 @@ var ApiEndPoint = "http://133.242.53.17/";
 $(".UserRegistSubmit").on("click", UserRegistSubmit);
 
 $(".user-delete").on("click",function(){
-  $("#UserDeleteConfirm").modal("show");
+  $("#DeleteUserConfirm").modal("show");
 });
+
+$(".delete-user-modal-button").on("click",UserDeleteSubmit);
 
 
 function UserRegistSubmit() {
@@ -29,6 +31,30 @@ function UserRegistSubmit() {
             }
         }
     });
+}
+
+function UserDeleteSubmit(){
+  var usernameForm = $("#delete-user-username")
+  var passwordForm = $("#delete-user-password")
+  $.ajax({
+      type: "DELETE",
+      url: ApiEndPoint + "user",
+      cache: false,
+      data: {
+          username: usernameForm.val(),
+          password: passwordForm.val()
+      },
+      dataType: "json",
+      success: function(data) {
+          if (data.status) {
+              logout();
+          } else {
+              $(".err-msg").removeClass("hidden");
+              usernameForm.parent("div").addClass("has-error");
+              passwordForm.parent("div").addClass("has-error");
+          }
+      }
+  });
 }
 
 function UserActivate(username, password) {
