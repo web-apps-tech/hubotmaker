d9225ead74758090ec2225a5aa42aa8c33318364
chamaharun <chamaharun@outlook.com>
2016-07-29 22:59:09 +0900

    ,

--- frontend/login.js
+++ frontend/login.js
@@ -35,7 +35,7 @@ function LoginError(){
 
 
 
-$("#Password").on("keydown", function(e) {
+$("#Username, #Password").on("keydown", function(e) {
 	if(e.keyCode === 13) {
 		$(".LoginButton").trigger("click");
 	}
