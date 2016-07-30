465fa22b10aaababc81013c0928dda37625d4a13
chamaharun <chamaharun@outlook.com>
2016-07-30 16:51:22 +0900

    template divide

--- frontend/list/index.pl
+++ frontend/list/index.pl
@@ -3,17 +3,25 @@
 use HTML::Template;
 use CGI qw/:standard/;
 
-my $api_key = param("api-key");
 
-my $template = HTML::Template->new(filename => 'index.tmpl',
+my $html1 = HTML::Template->new(filename => '../html1.tmpl',
+                                'die_on_bad_params' => 0
+                                );
+my $head = HTML::Template->new(filename => '../head.tmpl',
+                                'die_on_bad_params' => 0
+                                );
+my $navbar = HTML::Template->new(filename => '../navbar.tmpl',
+                                'die_on_bad_params' => 0
+                                );
+my $list = HTML::Template->new(filename => '../list.tmpl',
                                 'die_on_bad_params' => 0
                                 );
 
-$template->param(APIKEY => param("api-key") );
 print header(-type => "text/html",
              -status => 200,
     	 -charset=>'utf-8',
     	 -Access_Control_Allow_Origin=>'*'),
-      $template->output;
-
-
+      $html1->output,
+      $head->output,
+      $nabvar->output,
+      $list->output;
