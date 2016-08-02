c3404dafadd2d8670dabb6fce08a280e4432b2ca
nasa9084 <nasa9084@live.jp>
2016-08-03 00:39:32 +0900

    add: service file

---
+++ backend/scripts/hubotmaker.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/sh
+
+if [ ! -e /var/run/uwsgi ]; then
+    mkdir /var/run/uwsgi
+fi
+
+if [ ! -e /var/run/plack ]; then
+    mkdir /var/run/plack
+fi
+
+export PATH="/root/.anyenv/envs/pyenv/shims/:$PATH"
+export PERL_LOCAL_LIB_ROOT="$PERL_LOCAL_LIB_ROOT:/root/perl5"
+export PERL_MB_OPT="--install_base /root/perl5"
+export PERL_MM_OPT="INSTALL_BASE=/root/perl5"
+export PERL5LIB="/root/perl5/lib/perl5:$PERL5LIB"
+export PATH="/root/perl5/bin:$PATH"
+
+cd /root/app
+uwsgi uwsgi.ini
+
+cd /var/www/haas-front
+plackup -s FCGI --listen /var/run/plack/haas-front.sock --pid /var/run/plack/haas-front.pid -a scripts/haas.psgi
