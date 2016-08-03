db251df2c0130cbbc781479a72d0d8cedd6f6af0
nasa9084 <nasa9084@live.jp>
2016-08-04 01:06:25 +0900

    fix: PATH

--- usr/libexec/hubotmaker/hubotmaker.sh
+++ usr/libexec/hubotmaker/hubotmaker.sh
@@ -13,7 +13,7 @@ export PERL_LOCAL_LIB_ROOT="$PERL_LOCAL_LIB_ROOT:/root/perl5"
 export PERL_MB_OPT="--install_base /root/perl5"
 export PERL_MM_OPT="INSTALL_BASE=/root/perl5"
 export PERL5LIB="/root/perl5/lib/perl5:$PERL5LIB"
-export PATH="/root/perl5/bin:$PATH"
+export PATH="/root/.anyenv/envs/plenv/shims:$PATH"
 
 cd /root/hubotmaker/apps
 uwsgi uwsgi.ini
